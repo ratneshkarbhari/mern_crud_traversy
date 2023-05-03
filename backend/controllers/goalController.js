f3a6e076bf45a44756a92f845ca2a88eb1c8d87d
ratnesh <ratneshkarbhari18@gmail.com>
2023-05-03 12:20:04 +0530

    crud api complete

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,27 +1,56 @@
 const asyncHandler = require("express-async-handler")
+const Goal = require("../models/goalModel")
 
 const getGoals = asyncHandler(async(req,res) => {
-    res.status(200).json({message:"Get Goals"})
+
+    const goals = await Goal.find()
+    
+    res.status(200).json(goals)
+
 })
 
 const setGoal = asyncHandler(async(req,res)=>{
 
     if (req.body.text) {
+        const goal = await Goal.create({
+            text: req.body.text
+        })
+
+        res.status(200).json(goal)
         
     }else{
         res.status(400)
         throw new Error("Please add a text field")
     }
     
-    res.status(200).json({message:"set Goals"})
+
 })
 
 const updateGoal = asyncHandler(async(req,res)=>{
-    res.status(200).json({message:`Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,{
+        text: req.body.text
+    }, {
+        new : true
+    })
+    res.status(200).json(updatedGoal)
 })
 
 const deleteGoal = asyncHandler(async(req,res)=>{
-    res.status(200).json({message:`delete goal ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+    const updatedGoal = await Goal.findByIdAndDelete(req.params.id)
+    res.status(200).json({message:'Goal Deleted'})
+
+    
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
 const port = process.env.PORT || 5000
-
+const connectDB = require("./config/db")
 const  {errorHandler} = require("./middleware/errorMiddleware")
 
+connectDB()
 
 const app = express()
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
