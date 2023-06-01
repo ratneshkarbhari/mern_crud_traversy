5241e12e48ad594659a722441ee58dc64411ed65
ratnesh-ultraindia <ratnesh.karbhari@ultraindia.com>
2023-06-01 17:07:45 +0530

    react dashboard login and register empty pages made

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,18 @@
+import { useState,useEffect} from "react"
+
+function Register() {
+
+     
+
+  return (
+    <div>
+
+        <Form>
+
+        </Form>
+
+    </div>
+  )
+}
+
+export default Register
